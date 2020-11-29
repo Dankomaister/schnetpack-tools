96f38ec2820e7d5885e040ced0b3736060c844ca
Danko <danhed89@gmail.com>
2020-11-29 23:21:29 +0900

    test

--- setup.py
+++ setup.py
@@ -3,6 +3,17 @@ from setuptools import setup, find_packages
 setup(
 	name = 'schnetpack-tools',
 	version = '0.1',
+	author = 'Daniel Hedman',
+	author_email = 'daniel.hedman@ltu.se',
 	url = 'https://github.com/Dankomaister/schnetpack-tools',
-	packages = find_packages('src')
+	packages = find_packages('src'),
+	#package_data={'': ['neighbor_list_kernel.cl']},
+	#include_package_data = True,
+	install_requires = [
+		'pyopencl',
+		'numpy',
+		'schnetpack'
+	],
+	license = 'MIT',
+	description='Additional tools for SchNetPack.'
 )
