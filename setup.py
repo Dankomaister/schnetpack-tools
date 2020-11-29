72c0fc1e2877b9b9151fa7150b6dbfcfe3de9f2f
Danko <danhed89@gmail.com>
2020-11-29 23:13:48 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,8 @@
 from setuptools import setup, find_packages
+
+setup(
+	name = 'schnetpack-tools',
+	version = '0.1',
+	url = 'https://github.com/Dankomaister/schnetpack-tools',
+	packages = find_packages('src')
+)
