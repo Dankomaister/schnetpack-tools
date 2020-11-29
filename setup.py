c4b2fd835ef4933af676e880c20d21b0e5f0c5a2
Danko <danhed89@gmail.com>
2020-11-29 23:49:35 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
 	author_email = 'daniel.hedman@ltu.se',
 	url = 'https://github.com/Dankomaister/schnetpack-tools',
 	packages = find_packages('src'),
+	package_dir = {'': 'src'},
 	#package_data={'': ['neighbor_list_kernel.cl']},
 	#include_package_data = True,
 	install_requires = [
